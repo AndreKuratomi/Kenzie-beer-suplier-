215ff58416142da6e9b900a86d9e6b349afa9304
André Kuratomi <andrekuratomi@gmail.com>
2021-08-05 21:43:05 -0300

    API requisition drafted

--- src/components/Adega/index.jsx
+++ src/components/Adega/index.jsx
@@ -1,8 +1,25 @@
-import api from "../../services/api"
+import api from "../../services/api";
+
+import BeerCard from "../BeerCard";
 
 export const Adega = () => {
-
-    return(
-        
-    )
-}
+  return (
+    <section>
+      <h1>KENZIE BEER SUPLIER!</h1>
+      <p>Choose the appropriate beers for a perfect celebration:</p>
+      <ul>
+        {api
+          .get("/beers/")
+          .then((response) => {
+            const arr = response.data;
+            arr.map((elt) => (
+              <li>
+                <BeerCard elt={elt} />
+              </li>
+            ));
+          })
+          .catch((err) => console.log(err))}
+      </ul>
+    </section>
+  );
+};
